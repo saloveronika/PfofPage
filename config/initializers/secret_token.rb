ead8e9b84ef052169d2853b0547485f25206ed4c
saloveronika <myrailsproject@gmail.com>
2012-12-11 16:40:51 +0200

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theology::Application.config.secret_token = '37eff49eee244e7d55ef976484a8661d9ccdf7dd169ee27efd8df54ffb446c837062e0d4e1c1c5e3e11024671d3134f624c8de583251583134d5ae5e7c070e98'
